aeb0f8d4611316284c9b461fe01c0f32d619c536
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2025-09-23 13:45:16 +0700

    [MM-85] setup api member and group

---
+++ src/features/matching-member/apis/index.ts
@@ -0,0 +1,53 @@
+import api from "../../../utils"
+
+//Member
+export const getProfileId = async (id: number) => {
+    const response = await api.get(`/student-requests/${id}`)
+    return response.data
+}
+
+export const updateProfile = async (id: number, data: any) => {
+    const response = await api.put(`/student-requests/${id}`, data)
+    return response.data
+}
+
+export const deleteProfile = async (id: number) => {
+    const response = await api.delete(`/student-requests/${id}`)
+    return response.data
+}
+
+export const getProfile = async () => {
+    const response = await api.get("/student-requests")
+    return response.data
+}
+
+export const createProfile = async (data: any) => {
+    const response = await api.post("/student-requests", data)
+    return response.data
+}
+
+//Group
+export const getGroupId = async (id: number) => {
+    const response = await api.get(`/teams/${id}`)
+    return response.data
+}
+
+export const updateGroup = async (id: number, data: any) => {
+    const response = await api.put(`/teams/${id}`, data)
+    return response.data
+}
+
+export const deleteGroup = async (id: number) => {
+    const response = await api.delete(`/teams/${id}`)
+    return response.data
+}
+
+export const getGroup = async () => {
+    const response = await api.get("/teams")
+    return response.data
+}
+
+export const createGroup = async (data: any) => {
+    const response = await api.post("/teams", data)
+    return response.data
+}
