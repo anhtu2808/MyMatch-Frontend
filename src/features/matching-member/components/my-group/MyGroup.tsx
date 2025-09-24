45f38ba88752cebffac8a4807c4d235e488d3547
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2025-09-25 02:03:44 +0700

    [MM-85] delete group

--- src/features/matching-member/components/my-group/MyGroup.tsx
+++ src/features/matching-member/components/my-group/MyGroup.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./MyGroup.css";
 import GroupModalView from "../modal/GroupModalView";
 import GroupModalForm from "../modal/GroupModalForm";
-import { getGroupStudentId } from "../../apis";
+import { deleteGroup, getGroupStudentId } from "../../apis";
 import { useAppSelector } from "../../../../store/hooks";
 import Pagination from "../../../review/components/Pagination/Pagination";
+import { Modal } from "antd";
 
 export interface Course {
   id: number;
@@ -68,6 +69,7 @@ function MyGroup() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalElements, setTotalElements] = useState(0);
   const pageSize = 10;
+  const { confirm } = Modal;
         const handleOpenGroupModalView = (id: number) => {
         setOpenView(true);
         setSelectedId(id)
@@ -100,6 +102,23 @@ function MyGroup() {
       timeZone: "Asia/Ho_Chi_Minh"
     })
   }
+
+  const handleDelete = async (id: number) => {
+      confirm({
+        title: "Xác nhận xóa",
+        content: "Bạn có chắc chắn muốn xóa profile này không?",
+        okText: "Xóa",
+        cancelText: "Hủy",
+        okButtonProps: { danger: true }, // nút đỏ
+        onOk: async () => {
+          try {
+            await deleteGroup(id);
+          } catch (err) {
+            console.error("Error delete my profile", err);
+          }
+        },
+      });
+    };
 
   return (
     <div className="my-group-list">
@@ -171,7 +190,7 @@ function MyGroup() {
             <button className="btn-edit-my-group" onClick={() => handleOpenGroupModalForm(g.id)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
                Chỉnh sửa</button>
-            <button className="btn-delete-my-group">
+            <button className="btn-delete-my-group" onClick={() => handleDelete(g.id)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                Xóa nhóm</button>
             <button className="btn-detail-my-group" onClick={() => handleOpenGroupModalView(g.id)}>
