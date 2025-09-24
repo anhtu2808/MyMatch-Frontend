f2cee88b7328b1e80b5eb6aebc4fdb91d8eebc6a
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2025-09-24 19:59:03 +0700

    [MM-85] delete my profile

--- src/features/matching-member/components/my-profile/MyProfile.tsx
+++ src/features/matching-member/components/my-profile/MyProfile.tsx
@@ -120,7 +120,22 @@ function MyProfile() {
         fetchProfileByStudentId()
       }, [studentId, currentPage])
 
-  // 
+    const handleDelete = async (id: number) => {
+    confirm({
+      title: "Xác nhận xóa",
+      content: "Bạn có chắc chắn muốn xóa profile này không?",
+      okText: "Xóa",
+      cancelText: "Hủy",
+      okButtonProps: { danger: true }, // nút đỏ
+      onOk: async () => {
+        try {
+          await deleteProfile(id);
+        } catch (err) {
+          console.error("Error delete my profile", err);
+        }
+      },
+    });
+  };
 
   return (
     <div className="my-profile-list">
@@ -141,7 +156,7 @@ function MyProfile() {
               <button className="my-profile-btn-edit" onClick={() => handleOpenProfileModalForm(p.id)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
                 Chỉnh sửa</button>
-              <button className="my-profile-btn-delete" >
+              <button className="my-profile-btn-delete" onClick={() => handleDelete(p.id)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                 Xóa profile</button>
                 <button className="my-profile-btn-detail" onClick={() => handleOpenProfileModalView(p.id)}>
