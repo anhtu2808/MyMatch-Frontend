9d0c33148a6b7438f5402b84678e6c4db93e61e2
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2025-11-15 22:02:23 +0700

    Update Coin.tsx

--- src/features/coin/pages/Coin.tsx
+++ src/features/coin/pages/Coin.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getCoinAPI } from '../apis'
 import "./Coin.css"
 import { useAppSelector } from '../../../store/hooks'
+import { useNavigate } from 'react-router-dom'
 
 interface Coin {
     id: number,
@@ -9,15 +10,16 @@ interface Coin {
 }
 
 const Coin: React.FC = () => {
+    const navigation = useNavigate()
     const wallet = useAppSelector((state) => state.user.wallet);
   return (
-    <div className="coin-button">
+    <button className="coin-button" onClick={() => navigation("/payment")}>
       <div className="coin-text">
         {/* {coins?.coin?.toLocaleString()} <br /> */}
         {wallet !== null ? wallet?.toLocaleString() : '...'} <br />
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-dollar-sign-icon lucide-circle-dollar-sign"><circle cx="12" cy="12" r="10"/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/><path d="M12 18V6"/></svg>
-    </div>
+    </button>
   )
 }
 
